1c8bf7132fb834c7c60343b7bae608a18cd58980
haruhiko28 <not.hisano.soccer@gmail.com>
2021-12-20 23:28:08 +0900

    add 1-3

---
+++ introduction2/01/1-3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Java Programming";
+        System.out.println("文字列s1の3文字目以降は"+s1.substring(3)+"です");
+        System.out.println("文字列s1の3文字目〜8文字目は"+s1.substring(3,8)+"です");    
+    }
+}
